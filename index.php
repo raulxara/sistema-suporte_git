76fab8c3327b3be0d3ed5e866af8f1a1356e5a4a
Raul Nascimento Cruz <raul.nascc98@gmail.com>
2023-09-05 21:13:57 -0300

    protecao de dados

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php  
-	define('HOST','localhost');
-	define('DATABASE','suportplug');
-	define('USER','root');
+	define('HOST','');
+	define('DATABASE','');
+	define('USER','');
 	define('PASSWORD','');
 
 	define('BASE','http://localhost/suportplug/');
